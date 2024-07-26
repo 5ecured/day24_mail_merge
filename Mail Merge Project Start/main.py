dc2d55044f2dd5ca93d4cfd81ce87cf83aed0535
Edward <onirotto7585@pm.me>
2024-07-26 15:55:56 +0700

    mail merge finished

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,8 +1,18 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = '[name]'
+list = []
+
+with open("./Mail Merge Project Start/Input/Names/invited_names.txt") as invited_names:
+    names = invited_names.readlines() # ['Aang\n', 'Zuko\n', 'Appa\n', 'Katara\n', 'Sokka\n', 'Momo\n', 'Uncle Iroh\n', 'Toph']
+    for name in names:
+        list.append(name.replace('\n', ''))
+    # now list contains ['Aang', 'Zuko', 'Appa', 'Katara', 'Sokka', 'Momo', 'Uncle Iroh', 'Toph']
+
+with open("./Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter:
+    contents = letter.read()
+    for name in list:
+        new_letter = contents.replace(PLACEHOLDER, name)
+
+        # with open can also CREATE a new file because it does not already exist
+        with open(f'./Mail Merge Project Start/Output/ReadyToSend/letter_for_{name}.txt', mode='w') as completed_letter:
+            # but the newly created file is blank. so we have to write something
+            completed_letter.write(new_letter)
